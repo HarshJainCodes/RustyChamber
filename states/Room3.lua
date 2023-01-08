0249e4877626a8a121aacf33bc3552939d572d81
Harsh Jain <harshjain17may@gmail.com>
2023-01-08 22:40:35 +0530

    water tank

--- states/Room3.lua
+++ states/Room3.lua
@@ -38,11 +38,19 @@ function Room3:init()
     end
 
     -----------------------------------------WATER PIPELINE--------------------------------------------------------------------------------
-    self.waterPipeLine = PlacableItems(500, 700, 150, 100, love.graphics.newImage('assets/room3/cock_off.png'))
+    self.waterPipeLine = PlacableItems(450, 700, 150, 100, love.graphics.newImage('assets/room3/cock_off.png'))
+    self.waterPipeLine.removedDirt = false
+    self.waterPipeLine.wireOnPipeline = PlacableItems(450, 700, 150, 100, love.graphics.newImage('assets/room3/wire_on_cock.png'))
+    self.waterPipeLine.wireOnPipeline.alpha = 1
+
 
     --------------------------------------------WATER TANK----------------------------------------------------------
     self.waterTankInteractable = PlacableItems(850, 400, 262, 300, love.graphics.newImage('assets/room3/water_tank.png'))
+    self.waterTankPopup = PopupWindow(love.graphics.newImage('assets/room3/watertank_top_water.png'), 0.6, 0.6)
+    self.waterTankPopup.emptyWater = false
+    self.waterTankPopup.alpha = 1
 
+    self.waterTankPopupRevealed = PopupWindow(love.graphics.newImage('assets/room3/watertank_water_empty.png'), 0.6, 0.6)
     -----------------------------------------------SINK-----------------------------------------
     self.sinkInteractable = PlacableItems(230, 350, 200, 150, love.graphics.newImage('assets/room3/sink.png'))
 
@@ -52,10 +60,12 @@ function Room3:init()
     self.mirrorInteractable.alpha = 1
 
     ----------------------------------------------------------------------------------------------------------------------
-    self.modifiedV = moonshine(WINDOW_WIDTH, WINDOW_HEIGHT, moonshine.effects.modified_vignette)
+    self.modifiedV = moonshine(WINDOW_WIDTH, WINDOW_HEIGHT, moonshine.effects.modified_vignette).chain(moonshine.effects.boxblur)
     self.modifiedV.modified_vignette.radius = 150
     self.modifiedV.modified_vignette.opacity = 0
     -- self.modifiedV.disable("modified_vignette")
+    self.modifiedV.boxblur.radius = {20, 20}
+    self.modifiedV.disable("boxblur")
 end
 
 function Room3:update(dt)
@@ -75,6 +85,10 @@ function Room3:update(dt)
         self.waterDropletTimer = math.random(1, 3)
     end
 
+    if self.waterPipeLine.removedDirt then
+        self.waterPipeLine.wireOnPipeline.alpha = math.max(self.waterPipeLine.wireOnPipeline.alpha - dt, 0)
+    end
+
 
     for key, value in pairs(self.allWaterDrops) do
         value.update(dt)
@@ -82,12 +96,46 @@ function Room3:update(dt)
 end
 
 function Room3:mousepressed(x, y, button, istouch)
-    if checkAABBCollision(x, y, self.ladderToRoom2) then
-        gStateMachine:change('room2')
-    end
+    if button == 1 then
+        if (not self.waterTankPopup.active) and (not self.waterTankPopupRevealed.active) then
+            -- if clicked on water tank
+            if checkAABBCollision(x, y, self.waterTankInteractable) then
+                self.modifiedV.enable("boxblur")
+                self.modifiedV.disable("modified_vignette")
+                self.waterTankPopup.active = true
+            end  
 
-    if checkAABBCollision(x, y, self.mirrorInteractable) then
-        self.mirrorInteractable.hidden = true
+            if checkAABBCollision(x, y, self.ladderToRoom2) then
+                gStateMachine:change('room2')
+            end
+
+            if checkAABBCollision(x, y, self.mirrorInteractable) then
+                self.mirrorInteractable.hidden = true
+            end
+
+            if not self.waterPipeLine.removedDirt and checkAABBCollision(x, y, self.waterPipeLine.wireOnPipeline) then
+                self.waterPipeLine.removedDirt = true
+            end
+        else
+            if self.waterTankPopup.active then
+                if CloseActivePopUpWindow(x, y, self.waterTankPopup) then
+                    self.modifiedV.disable("boxblur")
+                    self.modifiedV.enable("modified_vignette")
+                end
+
+                if checkAABBCollision(x, y, self.waterTankPopupRevealed) then
+                    self.waterTankPopupRevealed.active = true
+                    self.waterTankPopup.active = false
+                end
+            end
+
+            if self.waterTankPopupRevealed.active then
+                if CloseActivePopUpWindow(x, y, self.waterTankPopupRevealed) then
+                    self.waterTankPopupRevealed.active = false
+                    self.modifiedV.disable("boxblur")
+                end
+            end
+        end
     end
 end
 
@@ -109,12 +157,25 @@ function Room3:render()
             self.waterTankInteractable.render()
 
             self.waterPipeLine.render()
+            love.graphics.setColor(1, 1, 1, self.waterPipeLine.wireOnPipeline.alpha)
+            self.waterPipeLine.wireOnPipeline.render()
+            love.graphics.setColor(1, 1, 1, 1)
 
             for key, value in pairs(self.allWaterDrops) do
                 value.render()
             end
         end
     )
+
+    if self.waterTankPopup.active then
+        love.graphics.setColor(1, 1, 1, self.waterTankPopup.alpha)
+        self.waterTankPopup.render()
+        love.graphics.setColor(1, 1, 1, 1)
+    end
+
+    if self.waterTankPopupRevealed.active then
+        self.waterTankPopupRevealed.render()
+    end
 end
 
 function Room3:exit()
